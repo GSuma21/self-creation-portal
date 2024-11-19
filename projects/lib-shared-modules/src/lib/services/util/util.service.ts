942b6925652b1f43bb73c825e3e4d41cd5f262da
ramkumar-pacewisdom <ramkumar.n@pacewisdom.com>
2024-11-20 00:11:31 +0530

    cache issue fix

--- projects/lib-shared-modules/src/lib/services/util/util.service.ts
+++ projects/lib-shared-modules/src/lib/services/util/util.service.ts
@@ -54,7 +54,7 @@ export class UtilService {
   }
 
   downloadFiles(url:string) {
-    return this.http.get(url, { responseType: 'text' })
+    return this.http.get(url+`?cacheBuster=${new Date().getTime()}`, { responseType: 'text' })
   }
 
   getCommentList(resourceId:string|number){
