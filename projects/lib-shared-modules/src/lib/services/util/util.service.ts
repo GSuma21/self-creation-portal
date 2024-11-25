f8cd63a29667c471b0ced6a973775a33ca3101c6
GSuma21 <21gsuma@gmail.com>
2024-11-25 08:58:03 +0530

    bug- 2043 - Added JSON.parse(JSON.stringify(resourceData)) to create a deep copy of resourceData

--- projects/lib-shared-modules/src/lib/services/util/util.service.ts
+++ projects/lib-shared-modules/src/lib/services/util/util.service.ts
@@ -99,7 +99,8 @@ export class UtilService {
   }
 
 
-  removeEmptyKey(obj: any): Observable<any> {
+  removeEmptyKey(resourceData: any): Observable<any> {
+    let obj =  JSON.parse(JSON.stringify(resourceData));
     for (let key in obj) {
       if (Array.isArray(obj[key])) {
         obj[key] = obj[key].map((element: any) =>
