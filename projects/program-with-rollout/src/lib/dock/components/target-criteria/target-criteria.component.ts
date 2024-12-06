6621c13e31b53d4840bdff825653616f9cf23941
ramkumar-pacewisdom <ramkumar.n@pacewisdom.com>
2024-12-06 10:03:25 +0530

    target page fix

--- projects/program-with-rollout/src/lib/dock/components/target-criteria/target-criteria.component.ts
+++ projects/program-with-rollout/src/lib/dock/components/target-criteria/target-criteria.component.ts
@@ -16,38 +16,6 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import {MatTabsModule} from '@angular/material/tabs';
 
-  /** Constants used to fill up our data base. */
-  const FRUITS: string[] = [
-    'blueberry',
-    'lychee',
-    'kiwi',
-    'mango',
-    'peach',
-    'lime',
-    'pomegranate',
-    'pineapple',
-  ];
-  const NAMES: string[] = [
-    'Maia',
-    'Asher',
-    'Olivia',
-    'Atticus',
-    'Amelia',
-    'Jack',
-    'Charlotte',
-    'Theodore',
-    'Isla',
-    'Oliver',
-    'Isabella',
-    'Jasper',
-    'Cora',
-    'Levi',
-    'Violet',
-    'Arthur',
-    'Mia',
-    'Thomas',
-    'Elizabeth',
-  ];
 @Component({
   selector: 'lib-target-criteria',
   standalone: true,
@@ -133,12 +101,8 @@ export class TargetCriteriaComponent implements OnInit{
     sort!: MatSort;
 
     constructor(public dialogRef: MatDialogRef<TargetCriteriaComponent>, @Inject(MAT_DIALOG_DATA) public dialogData: any, private formService:FormService) {
-        console.log(this.dialogData)
-        const users = Array.from({length: 10}, (_, k) => createNewUser(k + 1));
-
         // Assign the data to the data source for the table to render
-        this.dataSource = new MatTableDataSource(users);
-
+        this.dataSource = new MatTableDataSource([{}]);
     }
 
     ngOnInit(): void {
@@ -259,21 +223,4 @@ export class TargetCriteriaComponent implements OnInit{
         this.dataSource.paginator.firstPage();
         }
     }
-}
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): any {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-    ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-    '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
-  };
-
 }
