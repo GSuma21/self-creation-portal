4ab0967a8458294b5061d9d81e302f67e26508c3
GSuma21 <21gsuma@gmail.com>
2024-12-16 12:10:32 +0530

    adding details of  functions

--- projects/program-with-rollout/src/lib/dock/components/resource-details/resource-details.component.ts
+++ projects/program-with-rollout/src/lib/dock/components/resource-details/resource-details.component.ts
@@ -274,6 +274,13 @@ export class ResourceDetailsComponent implements OnInit, OnDestroy {
 
   }
 
+  /**
+ * Handles click events triggered by controls. 
+ * Opens a dialog with predefined configurations and listens for the dialog close event.
+ * Currently supports the "targeting_criteria" control name.
+ * 
+ * @param control - The control object containing the name and associated data.
+ */
   onClickTriggeredParent(control: any) {
     switch (control.name) {
       case "targeting_criteria":
@@ -305,17 +312,22 @@ export class ResourceDetailsComponent implements OnInit, OnDestroy {
           },
         });
 
-        // Handle dialog closure
         dialogRef.afterClosed().subscribe((res: any) => {
           console.log('Dialog result:', res);
         });
-        break; // Exit switch after handling this case 
+        break;
       default:
-        break; // Default case for unmatched control names
+        break;
     }
   }
 
-  onActionTriggeredParent(control:any){
+  /**
+ * Handles action events triggered by controls. 
+ * Performs operations based on the "action" property of the control, such as "VIEW", "EDIT", or "DELETE".
+ * 
+ * @param control - The control object containing the action and associated item with an index.
+ */
+  onActionTriggeredParent(control:any){ 
     switch (control.action) {
       case "VIEW":
         break; 
@@ -324,7 +336,7 @@ export class ResourceDetailsComponent implements OnInit, OnDestroy {
       case "DELETE":
         break; 
       default:
-        break; // Default case for unmatched control names
+        break;
     }
 
   }
