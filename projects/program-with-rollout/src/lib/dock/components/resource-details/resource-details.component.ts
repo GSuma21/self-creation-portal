cfdf4784bf90e17b0793d233d635f505925c8363
ramkumar-pacewisdom <ramkumar.n@pacewisdom.com>
2025-01-02 22:53:42 +0530

    select targetting page fixes

--- projects/program-with-rollout/src/lib/dock/components/resource-details/resource-details.component.ts
+++ projects/program-with-rollout/src/lib/dock/components/resource-details/resource-details.component.ts
@@ -448,6 +448,7 @@ export class ResourceDetailsComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     if(this.programWithRolloutService.rolloutId){
+      this.programWithRolloutService.rollOutDetails.title = this.programWithRolloutService.rollOutDetails.title ? this.programWithRolloutService.rollOutDetails.title : this.programWithRolloutService.resourceDetails.title;
       this.programWithRolloutService.saveRollOut().subscribe((res)=> {
         let data = {
           message: 'SAVED_SUCCESSFULLY',
