b3dcb68f2b51b5adba10a8bbfe6d9274f3947bad
GSuma21 <21gsuma@gmail.com>
2025-01-29 16:42:57 +0530

    adding a permission in program resources

--- projects/program-with-rollout/src/lib/dock/components/program-resources/program-resources.component.ts
+++ projects/program-with-rollout/src/lib/dock/components/program-resources/program-resources.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormService, SOLUTION_LIST } from 'lib-shared-modules';
+import { ArrayContainsAllDirective, FormService, SOLUTION_LIST } from 'lib-shared-modules';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,13 +12,14 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'lib-program-resources',
   standalone: true,
-  imports: [ MatSidenavModule, MatButtonModule, MatIconModule, MatToolbarModule, MatListModule, MatCardModule,TranslateModule,],
+  imports: [ MatSidenavModule, MatButtonModule, MatIconModule, MatToolbarModule, MatListModule, MatCardModule,TranslateModule,ArrayContainsAllDirective],
   templateUrl: './program-resources.component.html',
   styleUrl: './program-resources.component.scss'
 })
 export class ProgramResourcesComponent {
   resourceList:any
   resource_count:any = 1;
+  permissions:any;
 
 constructor(private formService: FormService, private router:Router,){}
 
@@ -26,20 +27,24 @@ ngOnInit(){
   this.getsolutionList()
 }
 
-
 getsolutionList() {
-    this.formService.getPermissions().subscribe((res:any) => {
-      this.formService.getForm(SOLUTION_LIST).subscribe((form:any) =>{
-        this.resourceList = form?.result?.data?.fields?.controls
-        this.resourceList = this.formService.checkPermissions(this.resourceList,res.result)
-        let userRoles:any = localStorage.getItem('user_roles')
-        userRoles = JSON.parse(userRoles)
-        // if(!userRoles.find((item:any)=> item.title == 'content_creator')) {
-        //   this.router.navigate(['/home/up-for-review'])
-        // }
+  this.formService.getPermissions().subscribe((res:any) => {
+    this.formService.getForm(SOLUTION_LIST).subscribe((form) =>{
+      this.permissions = res.result;
+        this.resourceList = form?.result?.data?.fields?.controls.filter((item:any) => {
+          if(item.title != "PROGRAM") {
+            return item
+          }
       })
+      // this.resourceList = this.formService.checkPermissions(this.resourceList,res.result)
+      // let userRoles:any = localStorage.getItem('user_roles')
+      // userRoles = JSON.parse(userRoles)
+      // if(!userRoles.find((item:any)=> item.title == 'content_creator')) {
+      //   this.router.navigate(['/home/up-for-review'])
+      // }
     })
-  }
+  })
+}
 
 
   onCardClick(cardItem: any) {
