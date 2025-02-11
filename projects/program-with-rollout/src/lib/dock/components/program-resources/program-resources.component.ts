dfd6b49dca2ab225c947093e8c74cbef813c6585
GSuma21 <21gsuma@gmail.com>
2025-02-11 13:36:55 +0530

    bug: 2372

--- projects/program-with-rollout/src/lib/dock/components/program-resources/program-resources.component.ts
+++ projects/program-with-rollout/src/lib/dock/components/program-resources/program-resources.component.ts
@@ -124,9 +124,29 @@ createProgram() {
 }
 
 saveForm(){
-  if(this.programId){
+  if (!this.programId) {
     this.subscription.add(
-      this.programWithRolloutService.createOrUpdateProgram(this.programWithRolloutService.programData,this.programId).subscribe()
+      this.programWithRolloutService
+      .createOrUpdateProgram()
+      .subscribe((res: any) => {
+        (this.programId = res.result.id),
+          this.router.navigate([], {
+            relativeTo: this.route,
+            queryParams: {
+              programId: this.programId,
+              mode: modes.EDIT,
+            },
+            queryParamsHandling: 'merge',
+            replaceUrl: true,
+          });
+        this.programWithRolloutService.programData.id = res.result.id;
+      })
+    )
+  } else {
+    this.subscription.add(
+      this.programWithRolloutService
+      .updateProgramDraft(this.programId)
+      .subscribe()
     )
   }
 }
