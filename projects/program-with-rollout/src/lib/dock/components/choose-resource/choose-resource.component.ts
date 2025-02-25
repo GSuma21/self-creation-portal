e51da7aab30934686799b826d277fcd86487f41a
GSuma21 <21gsuma@gmail.com>
2025-02-25 14:44:32 +0530

    bug: 2364, 2370

--- projects/program-with-rollout/src/lib/dock/components/choose-resource/choose-resource.component.ts
+++ projects/program-with-rollout/src/lib/dock/components/choose-resource/choose-resource.component.ts
@@ -111,7 +111,7 @@ constructor(private httpService: HttpProviderService, private Configuration: Con
   }
 
 
-  getResourceList(type:any='',sort_by:any="",sort_order:any=""){
+  getResourceList(sort_by:any="",sort_order:any=""){
     const config = {
       url : this.Configuration.urlConFig.RESOURCE_LISTS_URLS.BASE + this.Configuration.urlConFig.RESOURCE_LISTS_URLS.ENDPOINTS.BROWSE_EXISTING_LIST,
       params : new URLSearchParams({ page: this.page.toString(), limit: this.limit.toString(), search:this.searchText ,sort_by:sort_by ? sort_by : this.sortBy,sort_order:sort_order ? sort_order : this.sortOrder , type:this.type ?this.type:'' })
@@ -203,6 +203,7 @@ constructor(private httpService: HttpProviderService, private Configuration: Con
   onSortOptionsChanged(event:any){
     this.sortBy = event.sort_by
     this.sortOrder = event.sort_order
+    console.log(event.sort_by,event.sort_order )
     this.page = 1
     this.subscription.add(
       this.getResourceList(event.sort_by,event.sort_order).subscribe((resourceList: any) => {
