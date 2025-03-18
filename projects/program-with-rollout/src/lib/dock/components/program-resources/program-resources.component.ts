53be50b85e32293a2c5779df53d9b10c114e44fa
GSuma21 <21gsuma@gmail.com>
2025-03-18 15:39:24 +0530

    showing resources in reverse order and send for review popup condition

--- projects/program-with-rollout/src/lib/dock/components/program-resources/program-resources.component.ts
+++ projects/program-with-rollout/src/lib/dock/components/program-resources/program-resources.component.ts
@@ -103,7 +103,7 @@ ngOnInit(){
   }
   if(this.programId && Object.keys(this.programWithRolloutService.programData)?.length > 1){
     this.resourceCount  =this.programWithRolloutService.programData.resources ?  this.programWithRolloutService.programData.resources?.length : 0;
-    this.resources = this.programWithRolloutService.programData.resources
+    this.resources = (this.programWithRolloutService.programData.published_on && (this.mode === solutionModes.RESOURCE_EDIT || this.mode === solutionModes.REVIEW  || this.mode === solutionModes.REQUEST_FOR_EDIT)) ? this.programWithRolloutService.programData.resources.slice().reverse():  this.programWithRolloutService.programData.resources
     this.addActionButtons()
     if ((this.programWithRolloutService?.programData?.stage == resourceStatus.REVIEW  || this.mode === solutionModes.REQUEST_FOR_EDIT || this.mode === solutionModes.REVIEWER_VIEW || this.mode === solutionModes.REVIEW || this.mode === solutionModes.CREATOR_VIEW) && (this.mode !== solutionModes.VIEWONLY)) {
       this.getCommentConfigs()
@@ -183,7 +183,7 @@ readProgram(){
         this.programWithRolloutService.setProgramData(res.result)
         this.programWithRolloutService.updateResourceTargetCriteria(this.programId)
         this.resourceCount  = this.programWithRolloutService.programData.resources ? this.programWithRolloutService.programData.resources.length : 0;
-        this.resources = this.programWithRolloutService.programData.resources
+        this.resources = (this.programWithRolloutService.programData.published_on && (this.mode === solutionModes.RESOURCE_EDIT || this.mode === solutionModes.REVIEW  || this.mode === solutionModes.REQUEST_FOR_EDIT)) ? this.programWithRolloutService.programData.resources.slice().reverse():  this.programWithRolloutService.programData.resources
         this.programWithRolloutService.tabValidationForProgram.programResources = res.result.resources?.length > 0 ? 'VALID' : 'INVALID'
         this.addActionButtons()
         this.programWithRolloutService.upDateProgramTitle()
--- projects/program-with-rollout/src/lib/dock/components/resource-level-targeting/resource-level-targeting.component.ts
+++ projects/program-with-rollout/src/lib/dock/components/resource-level-targeting/resource-level-targeting.component.ts
@@ -124,8 +124,7 @@ export class ResourceLevelTargetingComponent {
             if (Object.keys(this.programWithRolloutService.programData).length > 1) {
               this.resourceCount =
                 this.programWithRolloutService.programData.resources?.length;
-              this.resources =
-                this.programWithRolloutService.programData.resources;
+              this.resources = (this.programWithRolloutService.programData.published_on && (this.mode === solutionModes.RESOURCE_EDIT || this.mode === solutionModes.REVIEW  || this.mode === solutionModes.REQUEST_FOR_EDIT)) ? this.programWithRolloutService.programData.resources.slice().reverse():  this.programWithRolloutService.programData.resources
             } else {
               this.subscription.add(
                 this.programWithRolloutService
@@ -137,8 +136,7 @@ export class ResourceLevelTargetingComponent {
                     this.programWithRolloutService.upDateProgramTitle(res.result.title);
                     this.resourceCount =
                       this.programWithRolloutService.programData.resources?.length;
-                    this.resources =
-                      this.programWithRolloutService.programData.resources;
+                    this.resources = (this.programWithRolloutService.programData.published_on && (this.mode === solutionModes.RESOURCE_EDIT || this.mode === solutionModes.REVIEW  || this.mode === solutionModes.REQUEST_FOR_EDIT)) ? this.programWithRolloutService.programData.resources.slice().reverse():  this.programWithRolloutService.programData.resources
                   })
               );
             }
@@ -146,7 +144,7 @@ export class ResourceLevelTargetingComponent {
       );
       this.resourceCount =
         this.programWithRolloutService.programData.resources?.length;
-      this.resources = this.programWithRolloutService.programData.resources;
+      this.resources = (this.programWithRolloutService.programData.published_on && (this.mode === solutionModes.RESOURCE_EDIT || this.mode === solutionModes.REVIEW  || this.mode === solutionModes.REQUEST_FOR_EDIT)) ? this.programWithRolloutService.programData.resources.slice().reverse():  this.programWithRolloutService.programData.resources
       this.addResourceFields();
       if ((this.programWithRolloutService?.programData?.stage == resourceStatus.REVIEW  || this.mode === solutionModes.REQUEST_FOR_EDIT || this.mode === solutionModes.REVIEWER_VIEW || this.mode === solutionModes.REVIEW || this.mode === solutionModes.CREATOR_VIEW) && (this.mode !== solutionModes.VIEWONLY)) {
         this.getCommentConfigs()
@@ -269,7 +267,7 @@ export class ResourceLevelTargetingComponent {
       this.programWithRolloutService.programData.resources.map(
         (resource: any) => resource.id
       );
-    this.resources = this.programWithRolloutService.programData.resources;
+      this.resources = (this.programWithRolloutService.programData.published_on && (this.mode === solutionModes.RESOURCE_EDIT || this.mode === solutionModes.REVIEW  || this.mode === solutionModes.REQUEST_FOR_EDIT)) ? this.programWithRolloutService.programData.resources.slice().reverse():  this.programWithRolloutService.programData.resources
     this.addResourceFields();
     }else{
       this.subscription.add(
@@ -285,7 +283,7 @@ export class ResourceLevelTargetingComponent {
               this.programWithRolloutService.programData.resources.map(
                 (resource: any) => resource.id
               );
-            this.resources = this.programWithRolloutService.programData.resources;
+              this.resources = (this.programWithRolloutService.programData.published_on && (this.mode === solutionModes.RESOURCE_EDIT || this.mode === solutionModes.REVIEW  || this.mode === solutionModes.REQUEST_FOR_EDIT)) ? this.programWithRolloutService.programData.resources.slice().reverse():  this.programWithRolloutService.programData.resources
             this.addResourceFields();
           })
       );
