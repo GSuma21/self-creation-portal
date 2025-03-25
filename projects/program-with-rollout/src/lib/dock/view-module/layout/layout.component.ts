40341926e93de7c43edf92dc3167654dc9ac7073
GSuma21 <21gsuma@gmail.com>
2025-03-25 16:57:11 +0530

    changes for story  : 1432

--- projects/program-with-rollout/src/lib/dock/view-module/layout/layout.component.ts
+++ projects/program-with-rollout/src/lib/dock/view-module/layout/layout.component.ts
@@ -16,6 +16,7 @@ export class LayoutComponent {
   mode:any;
   sidenavData:any;
   saveRolloutData:boolean = true;
+  config:any
   constructor(private formService:FormService,  public programWithRolloutService:ProgramWithRolloutService, private utilService:UtilService,private dialog:MatDialog, private router:Router, private route:ActivatedRoute,private toastService:ToastService,private configuration: ConfigService, private sharedService: LibSharedModulesService){
     this.subscription.add(
       this.route.queryParams.subscribe((params: any) => {
@@ -25,13 +26,14 @@ export class LayoutComponent {
   }
   ngOnInit(){
     this.getData()
+    this.setConfig();
     this.subscription.add(
       this.programWithRolloutService.currentRolloutData.subscribe(data => {
         this.sidenavData= data?.sidenavData.sidenav
         this.headerData = data?.sidenavData.headerData
+        this.config = this.programWithRolloutService.programConfig
       })
     )
-    this.setConfig();
     this.utilService.saveComment = true;
   }
 
@@ -159,6 +161,7 @@ export class LayoutComponent {
         this.programWithRolloutService.saveProgramFunc(true);
         break;
       }
+      case "PUBLISH":
       case "SEND_FOR_REVIEW": {
         this.utilService.saveComment = false;
         this.programWithRolloutService.checkProgramSendForReviewValidation(true);
--- projects/program-with-rollout/src/lib/program-with-rollout.service.ts
+++ projects/program-with-rollout/src/lib/program-with-rollout.service.ts
@@ -360,7 +360,9 @@ export class ProgramWithRolloutService {
 
   triggerProgramSendForReview(){
     if(this.formMeta.formValidation.programDetails === 'VALID' && this.formMeta.formValidation.programResources === 'VALID' && this.formMeta.formValidation.resourceLevelTargeting === 'VALID'){
-       if ( this.programConfig?.show_reviewer_list &&
+       if ((!((!this.programData.published_on && !this.programConfig.review_required) ||
+           (this.programData.published_on && !this.programConfig.review_required_after_publish)
+             )) &&  this.programConfig?.show_reviewer_list &&
               !(this.programData.status === resourceStatus.REQUEST_FOR_CHANGES || this.programData.status === resourceStatus.PUBLISHED || this.programData.published_on))
              {
               this.getReviewerData().subscribe((list: any) => {
