190d7f7a150f9340fdc237987fd422b7779f68c7
GSuma21 <21gsuma@gmail.com>
2025-04-04 19:28:47 +0530

    bug: 2618, added new forms

--- projects/lib-project/src/lib/dock/components/project-details/project-details.component.ts
+++ projects/lib-project/src/lib/dock/components/project-details/project-details.component.ts
@@ -401,6 +401,7 @@ export class ProjectDetailsComponent implements OnDestroy, OnInit, AfterViewChec
           content: 'ADD_TITLE_TO_CONTINUE_SAVING',
           form:[this.formDataForTitle],
           exitButton: 'CONTINUE',
+          language:this.language
         },
       });
       return dialogRef
