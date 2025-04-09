0a18a547e9948b5bdc4148d98efbba55fe138194
GSuma21 <21gsuma@gmail.com>
2025-04-09 12:38:21 +0530

    changes for header

--- projects/lib-shared-modules/src/lib/components/header/header.component.ts
+++ projects/lib-shared-modules/src/lib/components/header/header.component.ts
@@ -48,7 +48,7 @@ export class HeaderComponent {
     const storedLanguage = JSON.parse(localStorage.getItem('preferred_language') ?? '{}')?.value ?? 'en';
     if (storedLanguage) {
         this.selectedLanguage = storedLanguage;
-        this.translateService.use(this.selectedLanguage);
+        this.translateService.use(storedLanguage);
     }
 
     this.subscription.add(
@@ -81,9 +81,11 @@ export class HeaderComponent {
 
   languageChange(event:any) {
     this.selectedLanguage = event.value;
-    this.translateService.use(this.selectedLanguage);
-    this.utilService.setNewLanguage(this.selectedLanguage)
-    this.utilService.setPreferredLanguage(event.value).subscribe()
+    this.translateService.use(event.value);
+    this.utilService.setNewLanguage(event.value)
+    this.utilService.setPreferredLanguage(event.value).subscribe((res:any) => {
+      localStorage.setItem('preferred_language', JSON.stringify({ value: this.selectedLanguage }));
+    })
   }
 
   ngOnDestroy() {
